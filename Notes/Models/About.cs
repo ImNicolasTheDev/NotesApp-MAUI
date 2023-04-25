f90da83572c0e3ca9ef3a5998519808d86fd05e2
ImNicolasTheDev <nicolas.blondeau0304@gmail.com>
2023-04-26 00:33:51 +0200

    General commit - Working :white_check_mark:

---
+++ Notes/Models/About.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Models;
+
+internal class About
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://github.com/ImNicolasTheDev";
+    public string Message => "Developed by ImNicolasTheDev (thanks to Microsoft).";
+}
